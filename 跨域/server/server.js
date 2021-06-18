a3fe7cfafec0a49e910685a8deb9ef694552d48d
xiaohuixu <xiaohuixu@126.com>
2021-06-18 10:43:10 +0800

    跨域学习提交

---
+++ 跨域/server/server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express()
+
+app.use((req,res,next)=>{
+  res.header("Access-Control-Allow-Origin","*")
+  res.header("Access-Control-Allow-Credential",true)
+  next()
+})
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+app.get("/list",(req,res)=>{
+  console.log("req",req.query)
+  const data = {
+    name:"张三",
+    age:12
+  }
+  let callbackFn = req.query.callback
+  let str = `${callbackFn}(${JSON.stringify(data)})`
+  res.send(str)
+})
+
+app.listen("3000",()=>{
+  console.log("server is running")
+})
