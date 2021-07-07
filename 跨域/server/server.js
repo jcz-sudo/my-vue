5780b8949ea553287e8d82ed8ab9a31d1e0c1bd4
jcz-sudo <70131290+jcz-sudo@users.noreply.github.com>
2021-07-07 16:54:30 +0800

    小update

--- 跨域/server/server.js
+++ 跨域/server/server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 
+const socket = require("socket")
+
 app.use((req,res,next)=>{
   res.header("Access-Control-Allow-Origin","*")
   res.header("Access-Control-Allow-Credential",true)
